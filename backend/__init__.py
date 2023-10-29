ebecfaf35fcebc107a97b141f14d2bcaf6ac1a11
John <johnclapper89@gmail.com>
2023-10-29 09:08:46 -0400

    first commit

---
+++ backend/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, g, request
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.blueprints.auth import auth
+    from app.blueprints.main import main
+
+    app.register_blueprint(auth)
+    app.register_blueprint(main)
+
+    CORS(app, supports_credentials=True)
+
+    return app
