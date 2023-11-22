48cc783070e129f29127fae20153ff2374d9f2f9
John <johnclapper89@gmail.com>
2023-11-22 11:00:29 -0500

    Adjusted the user Schema to accept a location instead of zipcode

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -4,12 +4,6 @@ class UserSchema(Schema):
     username = fields.Str(required=True, validate=validate.Length(min=3, max=50))
     email = fields.Email(required=True)
     password = fields.Str(required=True, validate=validate.Length(min=6))
-    zipcode = fields.Str(required=True, validate=validate.Length(min=5, max=10))
+    location = fields.Str(required=True)
     profile_picture = fields.Str()
 
-    @validates('zipcode')
-    def validate_zipcode(self, value):
-        if not ((value.isdigit() and len(value) == 5) or
-                (len(value) == 10 and value[5] == '-' and
-                 value[:5].isdigit() and value[6:].isdigit())):
-            raise ValidationError("Invalid ZIP code format.")
