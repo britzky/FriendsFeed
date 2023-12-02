8b824c4757088723c021323126bfd2c6d567d2df
John <johnclapper89@gmail.com>
2023-12-02 14:55:57 -0500

    Added caching and logging for yelp restaurant fetches

--- backend/services/yelp_api.py
+++ backend/services/yelp_api.py
@@ -1,8 +1,18 @@
 from flask import current_app
+from requests_cache import CachedSession
 import requests
 import logging
 
+
+
 def get_restaurants_by_location(location):
+    yelp_session = CachedSession(
+        cache_name='yelp_cache',
+        backend='sqlalchemy',
+        expire_after=3600,
+        connection=current_app.config['SQLALCHEMY_DATABASE_URI']
+    )
+
     # Base URL for the Yelp Fusion API
     api_url = f"https://api.yelp.com/v3/businesses/search?location={location}&categories=restaurants"
 
@@ -13,9 +23,14 @@ def get_restaurants_by_location(location):
     headers = {
         "Authorization": f"Bearer {api_key}"
     }
+    # log weather the response was from cache or yelp
+    if response.from_cache:
+        logging.info(f"Response for {location} from cache")
+    else:
+        logging.info(f"Response for {location} from Yelp")
 
     # Make the GET request to the Yelp API
-    response = requests.get(api_url, headers=headers)
+    response = yelp_session.get(api_url, headers=headers)
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -79,6 +94,12 @@ def get_restaurant_by_name(name, location):
 
 # get restaurant by id from yelp
 def get_restaurant_by_id(yelp_restaurant_id):
+    yelp_session = CachedSession(
+    cache_name='yelp_cache',
+    backend='sqlalchemy',
+    expire_after=3600,
+    connection=current_app.config['SQLALCHEMY_DATABASE_URI']
+    )
     # Base URL for the Yelp Fusion API
     api_url = f"https://api.yelp.com/v3/businesses/{yelp_restaurant_id}"
 
@@ -91,7 +112,14 @@ def get_restaurant_by_id(yelp_restaurant_id):
     }
 
     # Make the GET request to the Yelp API
-    response = requests.get(api_url, headers=headers)
+    response = yelp_session.get(api_url, headers=headers)
+
+    # log weather the response was from cache or yelp
+    if response.from_cache:
+        logging.info(f"Response for {yelp_restaurant_id} from cache")
+    else:
+        logging.info(f"Response for {yelp_restaurant_id} from Yelp")
+
 
     # Check if the request was successful
     if response.status_code == 200:
