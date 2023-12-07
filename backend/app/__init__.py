7c6591161fb2092c554562e7e39bf4ba679fede2
John <johnclapper89@gmail.com>
2023-12-07 09:32:20 -0500

    Adjusted CORS config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -35,7 +35,7 @@ def create_app(testing=False):
     app.register_blueprint(auth)
     app.register_blueprint(main)
 
-    CORS(app, origins=["http://localhost:5173", "https://friendsfeed.netlify.app"],
+    CORS(app, origins=["http://localhost:5173", "https://friendsfeed.netlify.app", "https://main--friendsfeed.netlify.app"],
          supports_credentials=True,
          allow_headers=["Content-Type", "Authorization",],
         )
